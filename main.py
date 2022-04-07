30d2950336f5cd5c857efc4e9524efca567d0359
vandad1901 <vandad1901@gmail.com>
2022-04-08 01:29:55 +0430

    Disabled userbot

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from pyrogram import idle
 
 import bots.shutUp as SU
-import bots.userBot as UB
+#import bots.userBot as UB
 import common
 import moduleHelps
 
 SU.app.start()
-UB.usr.start()
+#UB.usr.start()
 
 SU.app.send_message(common.owner_id, "Starting")
 print("Starting")
@@ -15,7 +15,7 @@ idle()
 
 SU.app.send_message(common.owner_id, "Stopping")
 print("Stopping")
-print(UB.usr.export_session_string())
+#print(UB.usr.export_session_string())
 
 SU.app.stop()
-UB.usr.stop()
+#UB.usr.stop()
