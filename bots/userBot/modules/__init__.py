6cd27c3ae6905a231d09dd87dd627bb827db5440
vandad1901 <vandad1901@gmail.com>
2023-02-04 03:46:39 +0330

    Deleted useless file

--- bots/userBot/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-import glob
-from os.path import basename, dirname, isfile, join
-
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = allFiles = [basename(
-    f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
